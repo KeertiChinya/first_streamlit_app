fc3fdc704d14a01a4f04a8fc5871184f628d86c5
KeertiChinya <145023876+KeertiChinya@users.noreply.github.com>
2023-09-14 18:53:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.heder('Braekfast Menu')
+streamlit.header('Braekfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 stremlit.text('Kale, Spanish, & Rocket Smoothie')
 stremlit.text('Hard-Boiled Free-Range Egg')
